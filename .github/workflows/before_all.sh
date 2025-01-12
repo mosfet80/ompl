9d033e9da7625915a8ccf3cafbc49297c9e24677
mosfet80 <realeandrea@yahoo.it>
2025-01-12 18:59:40 +0100

    Update macos name
    fix macos 13 distro name misalignment

--- .github/workflows/before_all.sh
+++ .github/workflows/before_all.sh
@@ -15,7 +15,7 @@ if [ "${build_os}" == "Linux" ]; then
     # so the OMPL build process picks it up and can make use of it during the
     # Python binding generation stage.
     ln -s /opt/python/pp310-pypy310_pp73/bin/pypy /usr/bin
-elif [ "${build_os}" == "Darwin" ]; then
+elif [ "${build_os}" == "Ventura" ]; then
     export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
     export HOMEBREW_NO_AUTO_UPDATE=1
     brew install \
